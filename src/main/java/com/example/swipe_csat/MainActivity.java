5e51c2874e15b84f997bbd3bd0331b6a470acb3d
Alexandr <sanchesterus6@gmail.com>
2024-10-04 16:33:05 +0400

    feauture: add button

--- src/main/java/com/example/swipe_csat/MainActivity.java
+++ src/main/java/com/example/swipe_csat/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.swipe_csat;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +10,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button firbutton = findViewById(R.id.firstbutton);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
