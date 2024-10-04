ec5c5750a9220011f818430b686a2b2d6246eebc
Alexandr <sanchesterus6@gmail.com>
2024-10-04 16:52:04 +0400

    feature: добавить прощание

--- src/main/java/com/example/swipe_csat/category.java
+++ src/main/java/com/example/swipe_csat/category.java
@@ -1,9 +1,6 @@
 package com.example.swipe_csat;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,21 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class category extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        Button firbutton = findViewById(R.id.firstbutton);
-
-        firbutton.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, category.class));
-        });
-
-        System.out.println("HI!");
-        System.out.println("Bye!");
+        setContentView(R.layout.activity_category);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
